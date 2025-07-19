8ea6cc62759082da66af7ab4280ba861fd442f2f
Marlen <marlenportu@gmail.com>
2025-07-19 20:28:47 +0200

    Add detection placeholder and tests

---
+++ detector.py
@@ -0,0 +1,19 @@
+"""FakeX account detection utilities."""
+
+from typing import List
+
+
+def detect_fake_accounts(images: List[str]):
+    """Analyze account images to detect fake profiles.
+
+    Parameters
+    ----------
+    images : list[str]
+        Paths to account images.
+
+    Raises
+    ------
+    NotImplementedError
+        This function is a placeholder and has not yet been implemented.
+    """
+    raise NotImplementedError("Detection not implemented yet")
